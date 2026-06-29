d9e634fdd7be256e6c9f285728a1dd8582b61920
Vishwakarma-Ashutosh <ashutoshvishwakarma253@gmail.com>
2026-06-29 22:23:17 +0530

    reversre array with user input and array size

---
+++ ReverseArray/ReverseArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter size of the array");
+        int size= scanner.nextInt();
+        int[] arr = new int[size];
+        int temp;
+
+        System.out.println("enter the element you want to reverse");
+        for (int i = 0; i < size; i++) {
+            arr[i]=scanner.nextInt();
+
+        }
+        for(int i=0; i<arr.length/2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        System.out.println("reversed array");
+        for (int num:arr){
+            System.out.println(num+"");
+        }
+
+    }
+
+}
